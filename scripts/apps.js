3a8218f540e6efd52c37af4662c6b67b3f53c1de
ben <bholsinger@gmail.com>
2019-05-09 16:35:54 -0600

    logic and structure created

---
+++ scripts/apps.js
@@ -0,0 +1,62 @@
+/*
+thought process
+start with naming variables to store elements working on
+
+Elements working on:
+ * pet dog button
+ * give dog treat button
+ * dog stats <p> mood
+ * dog stats <p> number of pets
+
+
+
+*/
+
+let petButton = document.getElementById('petButton');
+let giveButton = document.getElementById('giveButton');
+
+
+/*
+create dog object
+key : value
+name : nameValue - string
+mood : moodValue-- string
+number of pets : 0 -- number value
+mood index: 1 -- number value
+tolerance level : 0 -- number value
+images : store sources for images
+
+
+*/
+
+/*
+draw function
+sets values for dog object
+changes image
+
+*/
+
+
+/*
+pet dog function
+name of function petDog
+this changes mood index by 1
+this changes tolerance level by 1
+this also when conditions are met :
+      * sets mood to next mood value which are strings
+      * adds 1 to number of pets
+
+*/
+
+
+/*
+give dog treat function
+name of function giveTreat
+this should keep dog at happiest level
+this sets num of pets to string "no pets needed, happiest dog ever"
+this sets mood to string "happiest dog ever, bestest dog ever"
+Name sets to Merline
+Picture ends at merlin
+
+*/
+
